6d945fe208c22710cc636deedf6a15ae9d68ce0a
Egor Orlenok <messaf@mail.ru>
2023-02-23 18:37:58 +1000

    fix game

--- GBTask5_2.py
+++ GBTask5_2.py
@@ -9,33 +9,30 @@
 import os
 os.system('cls') # Clear Terminal
 
-
 TOTAL_SWEETS = 100
 MAX_SWEETS_INPUT = 28
-# MAX_PLAYERS = 2
-
 
 def inputPlayer(player_num:int, ply_cur_num:int , max_input:int, cur_balance:int):
-    input_error = False
-    in_num = int()
-    while cur_balance > 0:
-        in_num = input(f'Ход игрока № {player_num}, у вас {ply_cur_num} конфет:')
-        if(in_num.isalnum()):
-            in_num = int(in_num)
-            if(in_num > max_input):
-                print(f'Неверный ввод, максимальное число {max_input} !!!')
-                input_error = True
-        else:
+    input_error = True # for do one cycle
+    in_num = None
+    while input_error:
+        in_num = input(f'Ход игрока № {player_num}, (Ваших конфет: {ply_cur_num}) введите число: ')
+        if(not in_num.isdigit()):
             print(f'Неверный ввод, введите число!!!')
             input_error = True
-        
-        if not input_error: break
-    else:
-       print(f'Конфет больше нету!!! ({cur_balance})')  
-       cur_balance = 0
-       return cur_balance
-            
+        elif(int(in_num)  > max_input):
+            print(f'Неверный ввод, максимальное число {max_input} !!!')
+            input_error = True
+        else:    
+            in_num = int(in_num)      
+            input_error = False
+
     cur_balance = (cur_balance - (ply_cur_num + in_num))
+    
+    if(cur_balance < 0): 
+        cur_balance = 0   
+        print(f'Конфет больше нету!!! ({cur_balance})')  
+       
     return cur_balance
 
 def play_game(mum_of_players:int):
@@ -46,17 +43,22 @@ def play_game(mum_of_players:int):
     print(player_list)
     
     while (player_winner_num == 0):
+        os.system('cls')
         print('=========================================================')
-        print(f'На столе осталось {sweets_balance} конфет')
+        print(f'Количество конфет на столе: {sweets_balance}')
         print('---------------------------------------------------------')
         for player in range(mum_of_players):
             move_number += 1
             sweets_balance = inputPlayer(player + 1, player_list[player], MAX_SWEETS_INPUT, sweets_balance)
             if sweets_balance == 0:
                 player_winner_num = player + 1
+                print('***************************************************')
                 print(f'Игра закончена, ход: {move_number}. \nИгрок № {player_winner_num} победитель!')
+                print('***************************************************')
+                break
         print('=========================================================\n')
         
-        return player_winner_num
-   
-play_game(2)
+    return player_winner_num
+ 
+# ========== Запуск Игры  ================= 
+play_game(2) # 2 игрока
