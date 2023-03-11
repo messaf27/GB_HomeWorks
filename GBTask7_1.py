02745a2018625a54fd044eb4ea2cd546b7c82e70
Egor Orlenok <messaf@mail.ru>
2023-03-12 00:56:58 +1000

    add Task7_1

---
+++ GBTask7_1.py
@@ -0,0 +1,46 @@
+
+import os
+clear_terminal = lambda: os.system('cls') # Clear Terminal
+
+rhythm_ok = "парам-пам-пам"
+rhythm_bad = "пам-парам"
+
+# Constants
+VOVELS_LIST = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
+NUM_OF_VOVEL = 4
+
+
+def is_vowel(char):
+    for elem in VOVELS_LIST:
+        if(elem == char):
+            return True
+    return False
+
+def rhythm_str_is_ok(rtm_str, num_vowel):
+    counter = 0 
+    sym_list = list(rtm_str)
+    for sym in sym_list:
+        if(is_vowel(sym)):
+            counter +=1
+    if(counter == num_vowel):
+        return True
+    
+    return False        
+
+def input_rhythm_process(in_str_list:list):
+    result = False
+    for curr_str in in_str_list:
+      if not rhythm_str_is_ok(curr_str, NUM_OF_VOVEL):
+          return False
+    
+    return True  
+
+# ======== Main Program ==================== #
+clear_terminal()
+input_list = input("Введите рифму: ").split()
+if(input_rhythm_process(input_list)):   
+    print(rhythm_ok)
+else:
+    print(rhythm_bad)
+
+# пум-пурум-пам
