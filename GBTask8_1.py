a4e628bf3c7645fc8a20c10c8df5fbff90bfe349
Egor <messaf@mail.ru>
2023-03-12 17:31:16 +1000

    add task 7

---
+++ GBTask8_1.py
@@ -0,0 +1,23 @@
+
+def menu():
+    pass
+
+
+def write_data():
+    pass
+
+
+def read_data():
+    pass
+
+
+def main():
+    pass
+
+def screen():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
